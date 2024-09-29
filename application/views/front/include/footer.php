6eee9f84d92b8dc13365129e2a597bb3dd84a7e9
Ankan Ghosh <ankanghosh010@gmail.com>
2024-09-29 14:46:32 +0530

    update footer

--- application/views/front/include/footer.php
+++ application/views/front/include/footer.php
@@ -99,8 +99,8 @@
     <div class="container pt-10 pb-10">
         <div class="row">
             <div class="col-md-12">
-                <h5 class="font-18 text-white-777 m-0 text-center pb-10 city_loc" style="color: white;font-family: 'Open Sans', sans-serif !important;font-size: 18px;
-    font-weight: 500;">Locate Us :</h5>
+                <h5 class="font-18 text-white-777 m-0 text-center pb-10 city_loc line-bottom" style="color: white;font-family: 'Open Sans', sans-serif !important;font-size: 18px;
+    font-weight: 500;">Locate Us</h5>
             </div>
             <div class="col-md-12 text-center text-md-right city_loc">
                 <ul style="list-style: none; padding: 0; margin: 0;">
