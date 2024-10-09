4ef025cb9f01995b1f4decb637de0035a5b7c8ff
freelancer010 <freelance.work.ankan@gmail.com>
2024-10-09 05:32:38 +0200

    srvice details fixes

--- application/views/front/service_details.php
+++ application/views/front/service_details.php
@@ -1,16 +1,10 @@
-<html dir="ltr" lang="en">
-<head>
+    <meta name="description" content="Yogintra service details" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="YogIntra" />
+    <title>Yogintra Service details</title>
+</head>
 
-<!-- Meta Tags -->
-<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="YogIntra" />
-
-<!-- Page Title -->
-<title>your website</title>
-
+<body>
   <!-- Start main-content -->
   <div class="main-content">
     <!-- Section: inner-header -->
